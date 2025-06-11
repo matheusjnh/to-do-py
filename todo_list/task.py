5f643f376b30669d98a71b666605704d270c8689
Matheus Santos <matheusjnh@gmail.com>
2025-06-11 20:23:07 -0300

    now task completion status can be specified on class construction

--- todo_list/task.py
+++ todo_list/task.py
@@ -1,4 +1,4 @@
 class Task:
-    def __init__(self, description: str):
+    def __init__(self, description: str, is_completed=False):
         self.description = description
-        self.is_completed = False
+        self.is_completed = is_completed
